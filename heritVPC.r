a2ea8ea9afc14aaede9b49d27fcef644f211dc72
Julius Olaifa <60077454+juliusolaifa@users.noreply.github.com>
2023-10-30 09:48:45 -0500

    Update heritVPC.r

--- heritVPC.r
+++ heritVPC.r
@@ -8,14 +8,13 @@ herit.vpc.fitted_model <- function(x, ...) {
   # Extract parameters from the fitted model object
   params <- extract_params(x)
   # Call the function with the parameters
-  my_function.params(params, ...)
+  herit.vpc.params(params, ...)
 }
 
-herit.vpc.fitted_model <- function(x, ...) {
-  # Extract parameters from the fitted model object
-  params <- extract_params(x)
-  # Call the function with the parameters
-  my_function.params(params, ...)
+herit.vpc.params <- function(x, ...) {
+  # Implement the functionality using the parameters
+  # x is expected to be a list or similar structure containing the parameters
+  ...
 }
 
 fitted_model_obj <- structure(list(), class = "fitted_model")
