1ebc7907986f7059d3d8ed72a4bb0a9753013c41
Julius Olaifa <60077454+juliusolaifa@users.noreply.github.com>
2023-10-30 10:00:52 -0500

    Update heritVPC.r

--- heritVPC.r
+++ heritVPC.r
@@ -8,7 +8,9 @@ herit.vpc.default <- function(x, ...) {
 
 herit.vpc.fitted_model <- function(x, ...) {
   # Extract parameters from the fitted model object
-  params <- extract_params(x)
+  params <- extract_params(x) 
+  #or
+  params <- x$coefficients
   # Call the function with the parameters
   herit.vpc.params(params, ...)
 }
