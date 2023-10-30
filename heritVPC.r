febc58b0af11abd5ca7637f521cea444d2912809
Julius Olaifa <60077454+juliusolaifa@users.noreply.github.com>
2023-10-30 18:25:29 -0500

    Update heritVPC.r
    make generic herit.vpc

--- heritVPC.r
+++ heritVPC.r
@@ -1,27 +1,73 @@
-herit.vpc <- function(X, Z=NULL, ...) {
-  UseMethod("herit.vpc", x)
+# Define the generic function
+herit.vpc <- function(X, Z=NULL, beta, Sigma, phi, family, g_inv.dist) {
+  UseMethod("herit.vpc")
 }
 
-herit.vpc.default <- function(X, Z=NULL, ...) {
-  stop("Unsupported input type")
+# Define the default method (direct parameter input)
+herit.vpc.default <- function(X,Z=NULL,beta,Sigma,phi,family,g_inv.dist) {
+    if(is.null(Z)) {
+        Z = X
+    }
+
+    # Check if X and beta have compatible dimensions
+    if (length(X) != length(beta)) {
+        stop("Dimensions of X and beta are not compatible for multiplication.")
+    }
+
+    # Check if X and beta have compatible dimensions
+    if (length(Z) != ncol(Sigma)) {
+        stop("Dimensions of Z and Sigma are not compatible for matrix multiplication.")
+    }
+    
+    mu <- X%*%beta
+    sigm <- Z%*%Sigma%*%Z
+    return(vpc_compute(mu, sigm, phi, family, g_inv.dist))
 }
 
-herit.vpc.fitted_model <- function(x, ...) {
+# Define the method for fitted_model input
+herit.vpc.fitted_model <- function(modelObj, X, Z=NULL) {
   # Extract parameters from the fitted model object
-  params <- extract_params(x) 
-  #or
-  params <- x$coefficients
-  # Call the function with the parameters
-  herit.vpc.params(params, ...)
+  beta <- modelObj$coefficients$beta
+  Sigma <- modelObj$coefficients$Sigma
+  phi <- modelObj$coefficients$phi
+  family <- modelObj$family
+  g_inv.dist <- modelObj$g_inv.dist
+  
+  # Call the default method with extracted parameters
+  herit.vpc.default(X, Z, beta, Sigma, phi, family, g_inv.dist)
 }
 
-herit.vpc.params <- function(x, ...) {
-  # Implement the functionality using the parameters
-  # x is expected to be a list or similar structure containing the parameters
-  ...
-}
+vpc_compute <- function(mu, sigm, phi, g_inv.dist, family, p=NULL) {
 
-extract_params <- function(x) {
+    if(g_inv.dist == "log") {
+        inv.mu <- exp(mu + sigm/2)
+        inv.var <- (exp(sigm) - 1)*exp(2*mu + sigm)
+    }
+    switch(family,
+           nbimom1={
+               return(inv.var / (inv.var + inv.mu + phi*inv.mu))
+           }
+           nbinom2={
+               inv.mu.p <- exp(2*mu + 4*sigm/2)
+               return(inv.var / (inv.var + inv.mu + inv.mu.p/phi))
+           },
+           tweedie={
+               if(is.null(p) || p <= 1 || p >= 2) {
+                   stop("p must be an integer between 1 and 2")
+               }
+               inv.mu.p <- exp(p*mu + p^2*sigm/2)
+               return(inv.var / (inv.var + phi*inv.mu.p))
+           }
+           compois={
+               return(inv.var / (inv.var + phi*inv.mu))
+           }
+           genpois={
+               return(inv.var / (inv.var + phi*inv.mu)) #phi = exp(eta)
+           }
+           {
+               return(paste("VPC not implemented for family: ", family))
+           }
+    )
 }
 
 fitted_model_obj <- structure(list(), class = "fitted_model")
