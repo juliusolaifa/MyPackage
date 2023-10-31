31718c732a8288eb888a8274b8c1be86ecd6abeb
Julius Olaifa <60077454+juliusolaifa@users.noreply.github.com>
2023-10-31 18:23:28 -0500

    Update heritTest.r

--- heritTest.r
+++ heritTest.r
@@ -8,28 +8,31 @@ herit.test.heritScore <- function(scoreObj, new_formula) {
 }
 
 herit.test.heritMod <- function(modObj, new_formula) {
-  modObj_extract <- modObj$modObj
-  L_H1 <- modObj$logLikelihood
-  total_par <- modObj$npar
-  total_varcomp <- modObj$npar - modObj$nfixef
-  npar_bounded <- floor((sqrt(1 + 8 * total_varcomp) - 1) / 2) # S = n(n+1)/2, where S =n(diag)
+  
+  full_modObj <- modObj$modObj
+  full_logLik <- full_modObj$logLikelihood
+  full_nfixef <- modObj$nfixef
+  full_nvarcomp_bounded <- modObj$nvarcomp_bounded
     
-  reduced_modObj <- update(modObj_extract, formula = new_formula)
-  L_H0 <- as.numeric(logLik(reduced_modObj))
-  total_reduced_npar <- attr(logLik(reduced_modObj), "df")
-  total_reduced_nfixef <- length(c(fixef(reduced_modObj)$cond, sigma(reduced_modObj)))
-  total_reduced_varcomp <- total_reduced_npar - total_reduced_nfixef
-  npar_reduced_bounded <- floor((sqrt(1 + 8 * total_reduced_varcomp) - 1) / 2)
+  reduced_modObj <- update(full_modObj, formula = new_formula)
+  reduced_logLik <- as.numeric(logLik(reduced_modObj))
+  reduced_nfixef <- reduced_modObj$nfixef
+  reduced_nvarcomp_bounded <- reduced_modObj$nvarcomp_bounded
 
-  nvarcomp_on_boundary <- npar_bounded - npar_reduced_bounded
   likStat <- -2*(L_H0 - L_H1)
 
   ###
-  critical_value <- compute_chisq_critical()
-  p_value <- 
+  pvalue_value <- compute_chisq_pvalue(full_nfixef, reduced_nfixef, full_nvarcomp_bounded, reduced_nvarcomp_bounded)
   # Return the results
   return(list(test_results = "Results of your test"))
 }
 
-compute_chisq_critical <- function() {
+compute_chisq_pvalue <- function(full_nfixef, reduced_nfixef, full_nvarcomp_bounded, reduced_nvarcomp_bounded) {
+    if(full_nfixef == reduced_nfixef) {
+      
+    }else if(full_nfixef = reduced_nfixef + 1) {
+      
+    }else{
+      stop("This case is not implemented)
+    }
 }
