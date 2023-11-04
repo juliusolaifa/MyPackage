c5f433ce2fc0c799901c4c22ecaa46bb3c6d1a0e
Julius Olaifa <60077454+juliusolaifa@users.noreply.github.com>
2023-11-04 08:54:06 -0500

    Update DataGen.r

--- DataGen.r
+++ DataGen.r
@@ -1,4 +1,4 @@
-herit.glmmdata <- function(x, beta, sigma.u, cluster.sizes, family, ...) {
+herit.glmmdata <- function(num.iterations, x, beta, sigma.u, cluster.sizes, family, ...) {
   
   # E(y|u) = log(xb + zu), u ~ N(0,E)
   cond_mean <- function() {
@@ -10,20 +10,29 @@ herit.glmmdata <- function(x, beta, sigma.u, cluster.sizes, family, ...) {
     z <- do.call(cbind, lapply(1:ncol(u), function(i) z * x[, i]))
     return(log(x%*%beta + z%*%c(u)))
   }
-  
-  #split conditional means into list according to group
-  cond_means <- split(cond_mean(), rep(1:length(cluster.sizes), cluster.sizes))
-    generate <- switch(family,
+
+  total_observations <- sum(cluster.sizes)
+  y_matrix <- matrix(nrow=num.iterations, ncol=total_observations)
+
+  generate <- switch(family,
            nbinom2=MASS::rnegbin,
-           #tweedie=function() tweedie::rtweedie(n=n, xmu = cond_mean, phi, power=NULL)
-           #compois=function() COMPoissonReg::rcmp(n=n, lambda, nu, control = NULL)
-           #genpois=function() HMMpa::rgenpois(n=n, lambda1, lambda2)
-           stop("data generation not implemented for family: ", family))
-  y <- unlist(mapply(generate, n=cluster.sizes, mu=cond_means, ...))
-  return(y)
-  #result <- structure(list("x" = x, "y" = y, "family" = family), class = "heritData")
-  #return(result)
+           tweedie=tweedie::rtweedie,
+           compois=COMPoissonReg::rcmp,
+           genpois=HMMpa::rgenpois,
+           stop("data generation not implemented for family: ", family)
+  )
+                               
+  for (i in 1:num.iterations) {                            
+      #split conditional means into list according to group
+      cond_means <- split(cond_mean(), rep(1:length(cluster.sizes), cluster.sizes))
+      y <- unlist(mapply(generate, n=cluster.sizes, mu=cond_means, ...))
+      y_matrix[i, ] <- y
+  }
+
+  result <- structure(list("x" = x, "y" = y_matrix, "family" = family), class = "heritData")
+  return(result)
 }
+                               
 
 # Example
 x <- c(23,24,25,26,29,30)
@@ -33,4 +42,5 @@ cluster.sizes <- c(3,2,1)
 n = 2
 family = "nbinom2"
 theta = 2
-herit.glmmdata(x, beta, sigma.u, cluster.sizes, family, theta)
+num.iterations =1
+herit.glmmdata(num.iterations, x, beta, sigma.u, cluster.sizes, family, theta)
