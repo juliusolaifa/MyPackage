d79d3bc6cf50526d3de1ff26995b70a300fed6f2
Julius Olaifa <60077454+juliusolaifa@users.noreply.github.com>
2023-11-04 21:09:56 -0500

    Update DataGen.r

--- DataGen.r
+++ DataGen.r
@@ -1,46 +1,74 @@
-herit.glmmdata <- function(num.iterations, x, beta, sigma.u, cluster.sizes, family, ...) {
-  
-  # E(y|u) = log(xb + zu), u ~ N(0,E)
-  cond_mean <- function() {
-    if(!is.matrix(sigma.u)) sigma.u <- as.matrix(sigma.u)
-    x <- model.matrix(~x)
-    u <- mvrnorm(n=length(cluster.sizes), mu=rep(0,ncol(sigma.u)), Sigma = sigma.u)
-    group <- factor(rep(1:length(cluster.sizes), cluster.sizes))
-    z <- model.matrix(~ 0 + group)
-    z <- do.call(cbind, lapply(1:ncol(u), function(i) z * x[, i]))
-    return(log(x%*%beta + z%*%c(u)))
-  }
+herit.glmmdata <- function(iter, x, beta, sigma.u, cluster.sizes, family, ...) {
+  sigma.u <- as.matrix(sigma.u)
+  x <- as.matrix(x)
+  if(nrow(x) != sum(cluster.sizes)) stop("number of rows of x must be equal to sum of cluster sizes")
+  x <- model.matrix(~x)
+  u <- mvrnorm(n=length(cluster.sizes), mu=rep(0,ncol(sigma.u)), Sigma = sigma.u)
+  group <- factor(rep(1:length(cluster.sizes), cluster.sizes))
+  z <- model.matrix(~ 0 + group)
+  z <- do.call(cbind, lapply(1:ncol(u), function(i) z * x[, i]))
 
   total_observations <- sum(cluster.sizes)
-  y_matrix <- matrix(nrow=num.iterations, ncol=total_observations)
+  y_matrix <- matrix(nrow=iter, ncol=total_observations)
 
+  cluster_assignments <- rep(paste0("strain", 1:length(cluster.sizes)), times=cluster.sizes)
+  cluster_assignments <- factor(cluster_assignments)
+                              
+  #split conditional means into list according to group
+  cond_means <- split(log(x%*%beta + z%*%c(u)), rep(1:length(cluster.sizes), cluster.sizes))
+                               
+  args <- list(...) 
   generate <- switch(family,
-           nbinom2=MASS::rnegbin,
-           tweedie=tweedie::rtweedie,
-           compois=COMPoissonReg::rcmp,
-           genpois=HMMpa::rgenpois,
+           nbinom2={
+             #MASS::rnegbin
+             function() unlist(mapply(MASS::rnegbin, n=cluster.sizes, mu=cond_means,args$theta))
+           },
+           tweedie={
+             #tweedie::rtweedie
+             function() unlist(mapply(tweedie::rtweedie, n=cluster.sizes, mu=cond_means, args$phi, args$power))
+           },
+           compois={
+             #COMPoissonReg::rcmp
+             function() unlist(mapply(COMPoissonReg::rcmp, n=cluster.sizes, lambda=cond_means, args$nu))
+           },
+           # genpois={
+           #   #HMMpa::rgenpois
+           #   lambda1 <- lapply(cond_means, function(cm) cm / (1 - args$lambda2))
+           #   function() unlist(mapply(HMMpa::rgenpois, n=cluster.sizes, lambda1=lambda1, args$lambda2))
+           # },
            stop("data generation not implemented for family: ", family)
   )
-                               
-  for (i in 1:num.iterations) {                            
-      #split conditional means into list according to group
-      cond_means <- split(cond_mean(), rep(1:length(cluster.sizes), cluster.sizes))
-      y <- unlist(mapply(generate, n=cluster.sizes, mu=cond_means, ...))
-      y_matrix[i, ] <- y
+
+                            
+  for (i in 1:iter) {                            
+      #y <- unlist(mapply(generate, n=cluster.sizes, mu=cond_means, ...))
+      y_matrix[i, ] <- generate()#y
   }
 
-  result <- structure(list("x" = x, "y" = y_matrix, "family" = family), class = "heritData")
+  result <- structure(list("x" = x, "y" = y_matrix, "family" = family, "cluster" = cluster_assignments), class = "heritData")
   return(result)
 }
-                               
+
+print.heritData <- function(dataObj) {
+  nx <- (ncol(dataObj$x))-1
+  datamat <- rbind(t(dataObj$x), dataObj$y)
+  colnames(datamat) <- dataObj$cluster
+  rownames(datamat) <- c(paste0('x', 0:nx), paste0('gene', 1:nrow(dataObj$y)))
+  return(datamat)
+}
 
 # Example
 x <- c(23,24,25,26,29,30)
-beta <- c(5,9)
+beta <- c(7,9)
 sigma.u <- 3#matrix(c(2,1,1,2),2)
 cluster.sizes <- c(3,2,1)
-n = 2
-family = "nbinom2"
 theta = 2
-num.iterations =1
-herit.glmmdata(num.iterations, x, beta, sigma.u, cluster.sizes, family, theta)
+phi = 2
+power = 1.6
+nu = 3
+lambda2 = 0.7
+iter =1
+data1 <- herit.glmmdata(iter, x, beta, sigma.u, cluster.sizes, "nbinom2", theta=theta)
+data2 <- herit.glmmdata(iter, x, beta, sigma.u, cluster.sizes, "tweedie", phi=phi, power=power)
+data3 <- herit.glmmdata(iter, x, beta, sigma.u, cluster.sizes, "compois", nu=nu)
+# herit.glmmdata(iter, x, beta, sigma.u, cluster.sizes, "genpois", lambda2=lambda2)
