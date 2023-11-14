c42c6e9dc49b487fde2a0361a85ca411a255a1e7
Julius Olaifa <60077454+juliusolaifa@users.noreply.github.com>
2023-11-14 11:59:38 -0600

    Update tryFigue.R

--- tryFigue.R
+++ tryFigue.R
@@ -1,51 +1,39 @@
-vcov.MixMod <- function (object, parm = c("all", "fixed-effects", "var-cov","extra", 
-                                          "zero_part"), sandwich = FALSE, ...) {
-    parm <- match.arg(parm)
+vcov.MixMod <- function (object) {
     V <- solve(object$Hessian)
-    if (sandwich) {
-        meat <- object$score_vect_contributions
-        ind <- !names(meat) %in% "score.D" & !sapply(meat, is.null)
-        meat[ind] <- lapply(meat[ind], rowsum, group = object$id[[1]], reorder = FALSE)
-        meat <- do.call('cbind', meat)
-        meat <- Reduce("+", lapply(split(meat, row(meat)), function (x) x %o% x))
-        V <- V %*% meat %*% V
-    }
-    if (parm == "all") {
-        return(V)
-    }
-    if (parm == "fixed-effects") {
-        n_betas <- length(object$coefficients)
-        return(V[seq_len(n_betas), seq_len(n_betas), drop = FALSE])
-    }
-    if (parm == "var-cov") {
-        D <- object$D
-        diag_D <- ncol(D) > 1 && all(abs(D[lower.tri(D)]) < sqrt(.Machine$double.eps))
-        include <- if (diag_D) {
-            unconstr_D <- log(diag(D))
-            n_betas <- length(object$coefficients)
-            seq(n_betas + 1, n_betas + length(unconstr_D))
-        } else {
-            unconstr_D <- chol_transf(D)
-            n_betas <- length(object$coefficients)
-            seq(n_betas + 1, n_betas + length(unconstr_D))
-        }
-        return(V[include, include, drop = FALSE])
-    }
-    if (parm == "extra") {
-        if (is.null(object$phis)) {
-            stop("the model behind 'object' contains no extra (phis) parameters.\n")
-        } else {
-            ind_phis <- grep("phi_", colnames(V), fixed = TRUE)
-            return(V[ind_phis, ind_phis, drop = FALSE])
-        }
-    }
-    if (parm == "zero_part") {
-        if (is.null(object$gammas)) {
-            stop("the fitted model does not have an extra zero part.")
-        } else {
-            gammas <- object$gammas
-            ind_gammas <- grep("zi_", colnames(V), fixed = TRUE)
-            return(V[ind_gammas, ind_gammas, drop = FALSE])
-        }
-    }
+    return(V)
 }
+
+# central difference vector (or matrix) for a function
+cd_vec <- function (x, f, ..., eps = 0.001) {
+    n <- length(x)
+    res <- matrix(0, n, n)
+    ex <- pmax(abs(x), 1)
+    for (i in seq_len(n)) {
+        x1 <- x2 <- x
+        x1[i] <- x[i] + eps * ex[i]
+        x2[i] <- x[i] - eps * ex[i]
+        diff.f <- c(f(x1, ...) - f(x2, ...))
+        diff.x <- x1[i] - x2[i]
+        res[, i] <- diff.f / diff.x
+    }
+    0.5 * (res + t(res))
+}
+
+
+#tht: These are the parameters of interest
+list_thetas <- list(betas = betas, D = if (diag_D) log(diag(D)) else chol_transf(D))
+    if (!is.null(phis)) {
+        list_thetas <- c(list_thetas, list(phis = phis))
+    }
+               
+tht <- unlist(as.relistable(list_thetas))
+
+Hessian <- cd_vec(tht, score_mixed, id = id, y = y, N = N, X = X, Z = Z, 
+                      offset = offset, X_zi = X_zi, Z_zi = Z_zi, offset_zi = offset_zi, 
+                      GH = GH, canonical = canonical, user_defined = user_defined, 
+                      Xty = Xty, Xty_weights = Xty_weights, log_dens = log_dens, mu_fun = mu_fun, 
+                      var_fun = var_fun, mu.eta_fun = mu.eta_fun, 
+                      score_eta_fun = score_eta_fun, score_eta_zi_fun = score_eta_zi_fun, 
+                      score_phis_fun = score_phis_fun, list_thetas = list_thetas, 
+                      diag_D = diag_D, penalized = penalized, pen_mu = pen_mu, 
+                      pen_invSigma = pen_invSigma, pen_df = pen_df, weights = weights)
