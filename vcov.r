82fd7baf63eab0de3ec17c814434c3ee6ececf7d
Julius Olaifa <60077454+juliusolaifa@users.noreply.github.com>
2023-11-14 22:21:45 -0600

    Update vcov.r

--- vcov.r
+++ vcov.r
@@ -9,17 +9,14 @@ vcov2 <- function(modObj) {
     sigma_f_ind <- nbeta + 1
     sigma_l_ind <- nbeta + nsigma 
     
-    chol_tocov <- function(theta) {
+    V_chol <- vcov(modObj)
+    V <- delta(theta_chol, V_chol, function(theta) {
         sigma <- theta[sigma_f_ind:sigma_l_ind]
         sigma <- GLMMadaptive:::chol_transf(sigma)
         sigma <- sigma[upper.tri(sigma, diag = TRUE)]
         theta[sigma_f_ind:sigma_l_ind] <- sigma
         theta
-    }
-    
-    J <- numDeriv::jacobian(chol_tocov, theta_chol)
-    V_chol <- vcov(modObj)
-    V <- J %*% V_chol %*% t(J)
+    }) 
     dimnames(V) <- dimnames(V_chol)
     theta <- chol_tocov(theta_chol)
     
@@ -38,14 +35,28 @@ vcov.heritMod <- function(modObj) {
         V1 <- V2 <- V
         V2[sigma_f_ind, ] <- V2[, sigma_f_ind] <- 0
         pi_1 <- 1; pi_2 <- 0.5
-        return(pi_1V1 + pi_2*V2)
+        V.combine <- pi_1V1 + pi_2*V2
     } else if(nsigma== 3) {
         pi_1 <- 0.25; pi_2 <- 0.25; pi_3 <- 0.25; pi_4 <- 0.25
         V1 <- V2 <- V3 <- V4 <- V
         V2[sigma_f_ind, ] <- V2[, sigma_f_ind] <- 0
         V3[c(sigma_f_ind+2), ] <- V3[, c(sigma_f_ind+2)] <- 0
         V4[c(sigma_f_ind,sigma_f_ind+2), ] <- V4[,c(sigma_f_ind,sigma_f_ind+2)] <- 0
-        return(pi_1*V1 + pi_2*V2 + pi_3*V3 + pi_4*V4)
+        V.combine <- pi_1*V1 + pi_2*V2 + pi_3*V3 + pi_4*V4
+    }
+    delta(theta,V.combine,****) 
+}
+
+delta <- function(estimates, cov_matrix, transformation) {
+    if (!is.vector(estimates) || !is.matrix(cov_matrix)) {
+        stop("Invalid inputs: 'estimates' must be a vector and 'cov_matrix' must be a matrix.")
+    }
+    grad <- numDeriv::jacobian(func = transformation, x = estimates)
+    transformed_var_cov <- grad %*% cov_matrix %*% t(grad)
+    if (length(transformation(estimates)) == 1) {
+        return(transformed_var_cov[1, 1])
+    } else {
+        return(transformed_var_cov)
     }
 }
 
